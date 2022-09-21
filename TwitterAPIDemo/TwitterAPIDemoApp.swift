56362f305af6ab191799f22be164eaabee0ea80d
IKEH1024 <pondau1024@gmail.com>
2022-09-21 16:18:49 +0900

    initial commit

---
+++ TwitterAPIDemo/TwitterAPIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAPIDemoApp.swift
+//  TwitterAPIDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAPIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
