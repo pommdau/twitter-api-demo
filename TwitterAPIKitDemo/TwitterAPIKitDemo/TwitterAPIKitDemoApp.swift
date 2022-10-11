72cb1b0ec66099434c88ed0329a771fc7819a6c9
IKEH1024 <pondau1024@gmail.com>
2022-10-11 17:13:30 +0900

    wip

---
+++ TwitterAPIKitDemo/TwitterAPIKitDemo/TwitterAPIKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAPIKitDemoApp.swift
+//  TwitterAPIKitDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAPIKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
